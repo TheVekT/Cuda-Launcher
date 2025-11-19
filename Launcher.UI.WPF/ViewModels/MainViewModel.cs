61e4ba35a44ff592a48eb6b1b666702cd60f6553
TheVekT <vviktor2007@gmail.com>
2025-11-19 20:06:17 +0200

    Update overlay system with binding improvements and command integration; adjust theme colors and remove redundant converter

--- Launcher.UI.WPF/ViewModels/MainViewModel.cs
+++ Launcher.UI.WPF/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 
 using System.ComponentModel;
+using System.Windows.Input;
+using Launcher.UI.WPF.Helpers;
 
 namespace Launcher.UI.WPF.ViewModels;
 
@@ -8,7 +10,9 @@ public class MainViewModel : INotifyPropertyChanged
 
 
     private double _uiScale = 1.0;
-
+    private bool _isOverlayVisible;
+    private object _currentOverlayView;
+    
     public double UiScale
     {
         get => _uiScale;
@@ -22,6 +26,52 @@ public class MainViewModel : INotifyPropertyChanged
         }
     }
     
+    
+    public bool IsOverlayVisible
+    {
+        get => _isOverlayVisible;
+        set
+        {
+            _isOverlayVisible = value;
+            OnPropertyChanged(nameof(IsOverlayVisible));
+        }
+    }
+    
+    public object CurrentOverlayView
+    {
+        get => _currentOverlayView;
+        set
+        {
+            _currentOverlayView = value;
+            OnPropertyChanged(nameof(CurrentOverlayView));
+            
+            IsOverlayVisible = _currentOverlayView != null;
+        }
+    }
+    
+    public ICommand CloseOverlayCommand { get; }
+    
+    public ICommand OpenSettingsCommand { get; }
+    
+    public MainViewModel()
+    {
+        OpenSettingsCommand = new RelayCommand(o => 
+        {
+            CurrentOverlayView = new Resources.Overlay.SettingsMenu();
+        });
+
+        /*
+        OpenLoginCommand = new RelayCommand(o => 
+        {
+            CurrentOverlayView = new Resources.Overlay.LoginMenu(); 
+        });*/
+
+        // Закрыть всё
+        CloseOverlayCommand = new RelayCommand(o => 
+        {
+            CurrentOverlayView = null;
+        });
+    }
     
     public event PropertyChangedEventHandler? PropertyChanged;
 
