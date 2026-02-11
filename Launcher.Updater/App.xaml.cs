520b2dd784e054b2ab6fb39d4d915e9c0c2003c2
TheVekT <vviktor2007@gmail.com>
2026-02-11 21:07:24 +0200

    Enhance Application Startup: override OnStartup to display Updater upon application launch.

--- Launcher.Updater/App.xaml.cs
+++ Launcher.Updater/App.xaml.cs
@@ -9,4 +9,12 @@ namespace Launcher.Updater;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        var updater = new MainWindow();
+        updater.Show();
+    }
+
 }
